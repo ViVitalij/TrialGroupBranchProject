26acce31c15ecadee7bf849a574fb7d16a4de8d1
ViVitalij <kontrabasista@wp.pl>
2017-03-06 23:19:48 +0100

    Cleaning code. Left only ConnectToDB class

--- src/main/java/Application.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by m.losK on 2017-03-04.
- */
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("Not to say Hello World!");
-	System.out.println("brabch test miki");
-    }
-}
